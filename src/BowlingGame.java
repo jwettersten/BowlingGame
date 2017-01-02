e5302559d2149e45f7f69e3453f909f3016bc015
Jon Wettersten <jwettersten@ideo.com>
2017-01-02 11:15:02 -0600

    remove newlines from eclipse

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -2,57 +2,54 @@
 public class BowlingGame {
 	private int currentRoll = 0;
 	private int rolls[] = new int[21]; // 21 potential rolls - no more than 3 in 10th frame
-	
+
 	public void roll(int numPinsKnockedDown) {
 		rolls[currentRoll] = numPinsKnockedDown;
-		
+
 		currentRoll++;
 	}
-	
+
 	public int score() {
 		int currentScore = 0;
-		
+
 		int frameIndex = 0;
-		
+
 		for (int frame = 0; frame < 10; frame++) {
-			
+
 			if (isSpare(frameIndex)) {
 				currentScore += 10 + spareBonus(frameIndex);
 				frameIndex += 2;
-				
+
 			} else if (isStrike(frameIndex)) {
 				currentScore += 10 + strikeBonus(frameIndex);
-				
+
 				frameIndex += 1;
-				
+
 			} else {
 				currentScore += sumOfPins(frameIndex);
-				
+
 				frameIndex += 2;
 			}
-			
-			
-		}
-		
+    }
 		return currentScore;
 	}
 
 	private boolean isSpare(int frameIndex) {
 		return rolls[frameIndex] + rolls[frameIndex+1] == 10;
 	}
-	
+
 	private boolean isStrike(int frameIndex) {
 		return rolls[frameIndex] == 10;
 	}
-	
+
 	private int spareBonus(int frameIndex) {
 		return rolls[frameIndex+2];
 	}
-	
+
 	private int strikeBonus(int frameIndex) {
 		return rolls[frameIndex+1] + rolls[frameIndex+2];
 	}
-	
+
 	private int sumOfPins(int frameIndex) {
 		return rolls[frameIndex] + rolls[frameIndex+1];
 	}
--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 
 public class BowlingGameTest {
-	
+
 	private BowlingGame game;
 
 	@Before
@@ -17,17 +17,17 @@ public class BowlingGameTest {
 	@After
 	public void tearDown() throws Exception {
 	}
-	
+
 	@Test
 	public void testGutterGame() {
-		
+
 		int numPins = 0;
 		int numRolls = 20;
-		
+
 		rollFramesWithNumPins(numPins, numRolls);
-		
+
 		assertEquals(0, game.score());
-		
+
 	}
 
 	private void rollFramesWithNumPins(int numPins, int numRolls) {
@@ -35,55 +35,55 @@ public class BowlingGameTest {
 			game.roll(numPins);
 		}
 	}
-	
+
 	@Test
 	public void testAllOnes() {
-		
+
 		int numPins = 1;
 		int numRolls = 20;
-		
+
 		rollFramesWithNumPins(numPins, numRolls);
-		
+
 		assertEquals(20, game.score());
-		
+
 	}
-	
+
 	@Test
 	public void testOneSpare() {
-		
+
 		rollSpare();
 		game.roll(3);
 		rollFramesWithNumPins(0, 17);
-		
+
 		assertEquals(16, game.score());
-		
+
 	}
 
 	private void rollSpare() {
 		game.roll(5);
 		game.roll(5);
 	}
-	
+
 	@Test
 	public void testOneStrike() {
-		
+
 		rollStrike();
 		game.roll(3);
 		game.roll(3);
 		rollFramesWithNumPins(0, 17);
-		
+
 		assertEquals(22, game.score());
-		
+
 	}
-	
+
 	private void rollStrike() {
 		game.roll(10);
 	}
-	
+
 	@Test
 	public void testPerfectGame() {
 		rollFramesWithNumPins(10, 12);
-		
+
 		assertEquals(300, game.score());
 	}
 
